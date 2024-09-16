f645de781f7f7b543c91ecce8b7b6a7b5a0a97fd
donShapoklyak <vholm368@gmail.com>
2024-09-16 16:32:04 +1000

    dgd

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ bot = telebot.TeleBot('7366753393:AAGmK2pf8x8Hw-xOdYevf59V_q9zE2sPaFs')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1)
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     webAppTest = types.WebAppInfo("https://donshapoklyak.github.io/head/")
-    one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest)
+    one_butt = types.KeyboardButton(text="воспользоватся конструктором", web_app=webAppTest)
     keyboard.add(one_butt)
-    bot.send_message(message.chat.id, "Hello!", reply_markup=keyboard)
+    bot.send_message(message.chat.id,"привет у нас ты можешь собрать собвственный пк!", reply_markup=keyboard)
     
 
 
 @bot.message_handler(content_types=['web_app_data'])
 def handle_web_app_data(message):
     data = message.web_app_data
-    bot.send_message(message.chat.id, data)
-
+    total_price = float(message.web_app_data.data)
+    bot.send_message(message.chat.id, f"Был выполнен заказ на сумму: {total_price} ₽")
 bot.polling()    
 
 """""
